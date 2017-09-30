5c8bd9da89afa756b36bc1533517b6dfdd75220a
papapapapapapapa <r3386588@gmail.com>
2017-09-30 14:42:12 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int x,y,z;
+        x=in.nextInt();
+        y=in.nextInt();
+        z=in.nextInt();
+        System.out.println(x+y+z);
+        System.out.println((x+y+z)/3);
+    }
+}
